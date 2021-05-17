99c0793fd9c882c576df89912523fa295f355b64
mariaDulesova <maria.dulesova@gmail.com>
2021-05-17 14:49:23 +0200

    set up vue

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Descrizione:
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un'immagine, presa anch'essa da un data.
+
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        message: "Welcome to Vue!", 
+    }
+    // method:   {
+    //     function () {
+    //     return {
+    //         images: ["https://www.travelfar.it/wp-content/uploads/pi%C3%B9-belle-spiagge-della-Corsica-Palombaggia.jpg"]
+    //     }
+    // }
+    
+})
+
+
+
