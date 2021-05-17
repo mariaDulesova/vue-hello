f9164a2db19b74df8a962b1231167596d4f23b23
mariaDulesova <maria.dulesova@gmail.com>
2021-05-17 15:14:01 +0200

    bonus with changing img

--- js/script.js
+++ js/script.js
@@ -9,6 +9,16 @@ var app = new Vue({
     data: {
         message: "Welcome to Vue!", 
         image: "https://www.travelfar.it/wp-content/uploads/pi%C3%B9-belle-spiagge-della-Corsica-Palombaggia.jpg"
+    },
+
+    methods: {
+        changeImage: function () {
+            if (this.image == "https://www.travelfar.it/wp-content/uploads/pi%C3%B9-belle-spiagge-della-Corsica-Palombaggia.jpg") {
+                this.image = "https://siviaggia.it/wp-content/uploads/sites/2/2020/09/milano-bosco-verticale-garibaldi-isola-design.jpg"
+            } else {
+                this.image ="https://www.travelfar.it/wp-content/uploads/pi%C3%B9-belle-spiagge-della-Corsica-Palombaggia.jpg"
+            }
+        }
     }
     
 })
